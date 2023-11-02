166edba2f58b0e882bfc374892aaf606b6a6df4f
AbdulazizAlghamdi24 <aalghamdi2952@stu.kau.edu.sa>
2023-11-02 08:42:25 +0300

    url

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-
+import React , { useState }from 'react';
 import './App.css';
 import Header from './Component/Header.js';
 import Main from './Component/Main.js';
 import Footer from './Component/Footer.js';
+
 function App() {
+  const [url, setUrl] = useState('');
 
-
+  const handleSubmit = () => {
+    
+    console.log("URL submitted:", url);
+  };
 
   return (
     <div className="App">
-
-      <Header/>
+      <Header />
       <Main
-      />
-      <Footer/>
+       url={url}
+        setUrl={setUrl} 
+        onSubmit={handleSubmit} />
+      <Footer />
     </div>
   );
 }
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,20 +1,24 @@
 import React from "react";
 import '../App.css'
 
-function Main (){
-
+function Main(props) {
+  
+  const handleInputChange = (event) => {
+    props.setUrl(event.target.value);
+  };
 
   return (
     <div className="Main--div">
       <label htmlFor="tinyUrl">Put Your URL Here: </label>
       <input
-       className="tinyUrl" 
-       type="text"
-       ></input>
-      <button > Submet</button>
-
+        className="tinyUrl"
+        type="text"
+        value={props.url}
+        onChange={handleInputChange}
+      />
+      <button onClick={props.onSubmit}>Submit</button>
     </div>
-  )
+  );
 }
 
 export default Main;
